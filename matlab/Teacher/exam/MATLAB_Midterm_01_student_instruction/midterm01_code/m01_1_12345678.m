5d6315eff82768229616884084e6c647bc022bfd
WaterSo0910 <jacky950141@gmail.com>
2020-04-21 22:04:01 +0800

    bello!!!

---
+++ matlab/Teacher/exam/MATLAB_Midterm_01_student_instruction/midterm01_code/m01_1_12345678.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Midterm Number: …
+% Problem number: …
+% Student Name:  …
+% Student ID: …
+% Email address: …
+% Department:
+% Date: ….
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all; clear; clc;		% close all windows
+                            % clear variables, and clear screen
+
+disp('Midterm Problem 1.1') 	% show Midterm Problem 1.1
+
+disp('Student Name; Student ID');
---
+++ matlab/Teacher/exam/MATLAB_Midterm_01_student_instruction/midterm01_code/m01_5.m
@@ -0,0 +1,41 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Midterm Number: 
+% Problem number: 
+% Student Name:  
+% Student ID: 
+% Email address: 
+% Department:
+% Date: 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Write down your answer in the file, m01_5.m. 
+%Change the file name to m01_5_yourID.m
+
+%Assume that all the variables are defined properly.  
+%Answer the questions in words. 
+%Do not write down any formulas or equations. 
+%Do not simply write down the values of the variables. 
+
+
+What is the purpose of each of the following code fragment?
+
+1) m(:, 2)
+Answer:
+
+2) find( z == 1 | z > 6)
+Answer:
+
+3) a( z < 0) = 11
+Answer:
+
+4) 10:2:10
+Answer:
+
+5) max(m(1,:))
+Answer:
+
+
+
+
+
