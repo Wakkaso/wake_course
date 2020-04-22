89724c4644c267aab7c91e864055e57caa5658b7
WaterSo0910 <jacky950141@gmail.com>
2020-04-23 00:48:01 +0800

    bello!!!

--- matlab/WaterSo/Lab02_Exercises_student_curves_1_hour/code/lab02_example.m
+++ matlab/WaterSo/Lab02_Exercises_student_curves_1_hour/code/lab02_example.m
@@ -54,12 +54,20 @@ disp('Lab Problem 2.1')
 t = 0;
 
 % grid
-p = [0 0];
-v = [15 20];
-dt = 0.02;
-m=1;
-g=2;
-n=[0 -1];
+
+t = 0;
+p1 = [0 0];
+v1 = [15 20];
+dt = 0.002;
+m1 = 1;
+g1 = 2;
+n1 = [0 -1];
+
+p2 = [0 0];
+v2 = [15 20];
+m2 = 1;
+g2 = 2;
+n2 = [0 -1];
 while(1)
     d = input('Please input d( , ): ');
     if (d(1)<0||d(2)<0) 
@@ -69,25 +77,47 @@ while(1)
         break;
     end    
 end
-
+fig = p1;
+fig2 = p2;
 while (t<=2) 
     clf
-    F = m*g*n;	% force. mg is the force magnitude. 
-    a1 = F/m - d(1)*v;
-    a2 = F/m - d(2)*v;  % acceleration,
-    v1 = v + a1 *dt;				% velocity
-    v2 = v + a2 *dt;
-
-    p1 = p + v *dt ;			% position
-    p2 = p + v *dt ;			% position
-    t = t + dt;				% time
-    %viscircles([p(1) p(2)], 1, 'color', 'b');
-
-    figure(1),plot(p1(1),p1(2));
-    figure(2),plot(p2(1),p2(2));
+    f = m1.*g1.*n1;
+    a = f./m1 - d(1).*v1;
+    v1 = v1 + a.*t;
+    p1 = p1 + v1.*t;
+    fig = [fig; p1];
     
+    f2 = m2.*g2.*n2;
+    a2 = f2./m2 - d(2).*v2;
+    v2 = v2 + a2.*t;
+    p2 = p2 + v2.*t;
+    fig2 = [fig2; p2];
+    
+    t = t + dt;				% time
+   
+    clf;
+    subplot(3,1,1);
+    plot(fig(:,1), fig(:,2),'-');
+    hold on;
+    viscircles([p1(1) p1(2)], 1, 'color', 'b');
+   % plot(p1(1), p1(2),'bo', 'linewidth', 3);
+    grid on;
     axis([0 400 -100 100]);
-    % pause(0.05);
+
+    subplot(3,1,2);
+    plot(fig2(:,1), fig2(:,2),'-');
+    hold on;
+    viscircles([p2(1) p2(2)], 1, 'color', 'b');
+    grid on;
+    axis([0 400 -100 100]);
+    
+    subplot(3,1,3);
+    plot(fig2(:,1), fig2(:,2),'-');
+    hold on;
+    viscircles([p2(1) p2(2)], 1, 'color', 'b');
+    grid on;
+    axis([0 400 -100 100]);
+    pause(dt);
 end
 
 
