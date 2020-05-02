23ebe15b7bcf3ce0e9a5dc47af2cdf7526180614
Chris Wang <bpploabc@gmail.com>
2020-05-02 15:21:42 +0800

    d

--- matlab/Teacher/homework/lab_brighten_up_an_image/lab_code/lab.m
+++ matlab/Teacher/homework/lab_brighten_up_an_image/lab_code/lab.m
@@ -1,11 +1,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem Category: ? exercise/lab/midterm/exam
-% Problem Number: ?
-% Student Name:  ?
-% Student ID: ?
-% Email Address: ?
-% Department: ?
-% Date: ?
+% Problem Category: lab
+% Problem Number: 1
+% Student Name:  Chris Wang
+% Student ID: 0716055
+% Email Address: bpploabc@gmail.com
+% Department: Computer Science
+% Date: 2020/05/04
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % close all windows
 % clear variables, and clear screen
@@ -13,4 +13,16 @@ close all; clf; clear; clc;
 
 % show a message about the problem in the exercise/lab/midterm/exam
 disp('Lab')  
+
+
+
+
+
+
+
+
+
+
+
+
 
