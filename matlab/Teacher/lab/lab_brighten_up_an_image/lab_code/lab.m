7cc746fc5c892b28748357e43a91fcc773a933f3
Chris Wang <bpploabc@gmail.com>
2020-05-08 13:17:23 +0800

    mat

---
+++ matlab/Teacher/lab/lab_brighten_up_an_image/lab_code/lab.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem Category: lab
+% Problem Number: 1
+% Student Name:  Chris Wang
+% Student ID: 0716055
+% Email Address: bpploabc@gmail.com
+% Department: Computer Science
+% Date: 2020/05/04
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% close all windows
+% clear variables, and clear screen
+close all; clf; clear; clc;
+
+% show a message about the problem in the exercise/lab/midterm/exam
+disp('Lab')  
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ matlab/Teacher/lab/lab_curve_interpolation_4_figs_5:11/lab_code/lab.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem Category: ? exercise/lab/midterm/exam
+% Problem Number: ?
+% Student Name:  ?
+% Student ID: ?
+% Email Address: ?
+% Department: ?
+% Date: ?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% close all windows
+% clear variables, and clear screen
+close all; clf; clear; clc;
+
+% show a message about the problem in the exercise/lab/midterm/exam
+disp('Lab')  
+
