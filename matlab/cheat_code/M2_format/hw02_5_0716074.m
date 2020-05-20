10e3127ac4c9ace8c64d0e99fdc0b62a7d645579
WaterSo0910 <jacky950141@gmail.com>
2020-05-21 02:27:18 +0800

    bello!!!

---
+++ matlab/cheat_code/M2_format/hw02_5_0716074.m
@@ -0,0 +1,37 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment Number: 1
+% Student Name: TSAI YU-CHENG
+% Student ID: 0716074
+% Email address: jacky950141@gmail.com
+% Department: Computer Science, NCTU
+% Date: 3/31
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clf; clear; clc;
+disp('problem 1');
+clf; clear; clc;
+fprintf('Student ID:0716074  Name:TSAI YU-CHENG\n');
+dt = 0.02;
+p0 = 10; v0 = 0; t0 = 0;  
+tmax = 10;
+figure
+hold on
+coef1 = input('Please input a in [0,1]: ');
+coef2= input('Please input dk in [0.1,1]: ');
+a = coef1(1);dk=coef2(1);
+for k =0:dk:1
+    p = p0; v = v0; t = t0; M = []; T = [];
+    while t < tmax
+        v = v - a*dt;
+        p = p + k*v*dt;
+        if p<0
+            p=0;
+            v=-v;
+        end
+        t = t + dt;
+        T = [T,t]; % expand T when necessary. This is slow.
+        M = [M,p]; % expand M when necessary. This is slow.
+    end
+    plot(T, M,'LineWidth',3);
+    hold on;
+    
+end
