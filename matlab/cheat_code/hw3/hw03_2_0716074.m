17659b47b57ce7af68ffda0e38f1834afd5b04f5
WaterSo0910 <jacky950141@gmail.com>
2020-05-20 23:46:07 +0800

    bello!!!

--- matlab/cheat_code/hw3/hw03_2_0716074.m
+++ matlab/cheat_code/hw3/hw03_2_0716074.m
@@ -7,27 +7,48 @@
 % Department: Computer Science, NCTU
 % Date: 5/8
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clf; clear; clc;		
+% clf; clear; clc;		
+% 
+% disp('Assignment Problem 3.2') 	% show Assignment Problem 3.2
+% T = 0 : 0.01 : 2*pi; 
+% R = cos(2.*T) .* sin(2.*T);
+% i = 1;
+% for t0 = T
+%     k=0:1;
+%     R0 = k.* R(1,i)./1 ;
+%     x = R0 .* cos(T(1,i));
+%     y = R0 .* sin(T(1,i));
+%     [theta,rho] = cart2pol(x,y);
+%     if(i>1)
+%     polarplot(last_t,last_r,'LineWidth',2,'Color','y');
+%     end
+%     last_t=theta;
+%     last_r=rho;
+%     polarplot(theta,rho,'LineWidth',2,'Color','b'); 
+%     polarplot(T(1:i),R(1:i),'LineWidth',2,'Color','b');
+%     
+%     hold on;     
+%     pause(0.001);
+%     i = i + 1;
+% end
 
-disp('Assignment Problem 3.2') 	% show Assignment Problem 3.2
-T = 0 : 0.01 : 2*pi; 
-R = cos(2.*T) .* sin(2.*T);
-i = 1;
-for t0 = T
-    k=0:1;
-    R0 = k.* R(1,i)./1 ;
-    x = R0 .* cos(T(1,i));
-    y = R0 .* sin(T(1,i));
-    [theta,rho] = cart2pol(x,y);
-    if(i>1)
-    polarplot(last_t,last_r,'LineWidth',2,'Color','y');
-    end
-    last_t=theta;
-    last_r=rho;
-    polarplot(theta,rho,'LineWidth',2,'Color','b'); 
-    polarplot(T(1:i),R(1:i),'LineWidth',2,'Color','b');
+
+close all; clf; clear; clc;
+
+disp('Assignment Problem 3.2')  
+
+hold on;
+for x = 0:0.01:2*pi
+    x0 = 0:0.01:x;
+    r0 = cos(2.*x0).*sin(2.*x0);
+    clf;
     
-    hold on;     
+    
+    h = polar(x0,r0);
+    hold on;
+    
+    [x1,y1] = pol2cart(x0,r0);
+    fill(x1,y1,'y');
+    polar(x0,r0,'b');
     pause(0.001);
-    i = i + 1;
 end
--- matlab/cheat_code/lab5_curve/lab02_2.m
+++ matlab/cheat_code/lab5_curve/lab02_2.m
@@ -27,34 +27,42 @@ fprintf("This system generates %d particles.  Enjoy the simulation.",n);
 M = 10000; m = 1; t = 0;
 
 p = zeros(n,2);
+v = zeros(n,2);
 pd = zeros(n,1);
 for k = 1 : n
     while 1
         p(k,:) = (rand(1,2)*100) - 50;  % -50~50
-        pd(k) = (p(k,1)^2 + p(k,2)^2) ^ (1/2);  % ||p||
-        if pd(k) <= 50 && pd(k) >= 10
+        pd = norm([p(k,1),p(k,2)]);  % ||p||
+
+        if pd <= 50 && pd >= 10
+            q = [-p(k,2) p(k,1)]; 
+            v(k,:) = 20.*q./pd; 
             break;
         end
+
     end
 end
 
 while 1
-    v = 20 * [p(:,2) -p(:,1)] ./ pd;
-
-    F = - p.*m.*M ./ ( pd .* (1+p.^2) );
-    a = F ./ m;
-    v = v + a .* 0.025;
-    p = p + v .* 0.025;
-    t = t + 0.025;
+    clf;
+    plot(0,0,'.b');
+    hold on;
+%     t = t + 0.025;
     for pk=1:n
+        
+        F = - p(pk,:).*m.*M ./ (norm(p(pk,:)) .* (1+norm(p(pk,:)).^2) );
+        a = F ./ m;
+        v(pk,:) = v(pk,:) + a .* 0.025;
+        p(pk,:) = p(pk,:) + v(pk,:) .* 0.025;
         cp0 = [ 0 0 1];  % blue
         cp1 = [ 1 0 0];  % red
-        vD = min(1, pd(pk,:)/50);
+        vD = min(1, norm(p(pk,:))./50);
         p_color  = cp0 + vD.*(cp1-cp0);
         plot(p(pk,1),p(pk,2),'o','color',p_color);
-        hold on;
-        axis equal square manual;
+        
     end
+    axis([-80 80 -80 80]);
+    pause(0.00000001);
 end
 
 
